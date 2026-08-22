78e5124ce55f16c6e21ce5f250c580a243046d69
Varaprasad <varaprasad1016@users.noreply.github.com>
2026-08-22 11:14:58 +0100

    Register local UpdateInstaller plugin explicitly in MainActivity

--- mobile/android/app/src/main/java/com/albumforge/app/MainActivity.java
+++ mobile/android/app/src/main/java/com/albumforge/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.albumforge.app;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(UpdateInstaller.class);
+        super.onCreate(savedInstanceState);
+    }
+}
